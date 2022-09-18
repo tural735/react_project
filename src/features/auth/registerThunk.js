657e156e07b306d7bea057061643bc1db1ae8a48
Tural Memmedzade <49388337+tural735@users.noreply.github.com>
2022-09-18 14:57:31 +0400

    check

--- src/features/auth/registerThunk.js
+++ src/features/auth/registerThunk.js
@@ -3,12 +3,7 @@ import customFetch from "../../utils/axios";
 
 export const registerUserThunk=async(url,user,thunkAPI)=>{
   try {
-    const resp=await customFetch.post(url,user,{
-      headers:{
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      }
-    });
+    const resp=await customFetch.post(url,user);
     console.log(resp.data);
     return resp.data;
   } catch (error) {
